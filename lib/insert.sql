0abd4b7f13c728f7aaea7a564b796b2128894151
matthew rendely <matthew.rendely@gmail.com>
2023-06-20 21:21:20 -0700

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES
+('Mr. Chocolate', 10, 'female', 'red', 'kind', 1),
+('Rowdy', 10, 'female', 'red', 'kind', 1),
+('Tabitha', 10, 'female', 'red', 'kind', 1),
+('Sergeant Brown', 10, 'female', 'red', 'kind', 1),
+('Melissa', 10, 'female', 'red', 'kind', 1),
+('Grinch', 10, 'female', 'red', 'kind', 1),
+('Wendy', 10, 'female', 'red', 'kind', 1),
+(NULL, 10, 'female', 'red', 'kind', 1)
